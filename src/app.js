fe9d21039b76133ec6fea7dc2a8672e913e01e37
Abolfazl <at9506270@gmail.com>
2026-06-20 23:43:11 +0330

    chore: setup initial project structure

---
+++ src/app.js
@@ -0,0 +1,11 @@
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Hello World!")
+})
+
+app.use(express.json());
+
+export default app;
---
+++ src/server.js
@@ -0,0 +1,6 @@
+import app from "./app.js";
+import "dotenv/config";
+
+const PORT = process.env.PORT;
+
+app.listen(PORT, console.log(`server is running on port ${PORT}`));
